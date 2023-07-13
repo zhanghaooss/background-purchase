4f108fb0db8f0358408e8426bbda29d3165c4c85
Yubei <zhanggeyu030@gmail.com>
2023-07-13 23:30:37 +0800

    ‘。。。’

---
+++ src/components/base/StoragePlan.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+      name: "StoragePlan",
+      data(){
+          return {
+
+          }
+      },
+      methods:{},
+      mounted(){
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
